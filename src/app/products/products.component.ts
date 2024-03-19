2e9ca025175901673a68bd5c999f2c0e6114567e
guiiiividd <guiiiividd@gmail.com>
2024-03-19 17:45:17 -0300

    :sparkles: feat: Save Products in the Table

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Product } from '../Product';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  products: Product[] = [];
 
+  formGroupProduct: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroupProduct = formBuilder.group({
+      id: [''],
+      name: [''],
+      description: [''],
+      price: [''],
+      quantity: [''],
+    });
+  }
+
+  save() {
+    this.products.push(this.formGroupProduct.value);
+  }
 }
