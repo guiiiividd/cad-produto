db50782cc7af6d61bf4ad2e8e0a10cb1bbe52914
guiiiividd <guiiiividd@gmail.com>
2024-04-02 22:49:33 -0300

    :sparkles: feat: Saving and Getting Products using json-server

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../Product';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
   formGroupProduct: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: ProductService
+  ) {
     this.formGroupProduct = formBuilder.group({
       id: [''],
       name: [''],
@@ -21,7 +25,19 @@ export class ProductsComponent {
     });
   }
 
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
   save() {
-    this.products.push(this.formGroupProduct.value);
+    this.service.save(this.formGroupProduct.value).subscribe({
+      next: (data) => this.products.push(data),
+    });
+  }
+
+  loadProducts() {
+    this.service.getProducts().subscribe({
+      next: (data) => (this.products = data),
+    });
   }
 }
